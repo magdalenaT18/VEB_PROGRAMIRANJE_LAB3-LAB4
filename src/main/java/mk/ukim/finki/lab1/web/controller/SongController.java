04423a656dc6f0f351b44c261366aff6975a4150
Magdalena Trajkova <magdalena.trajkova@students.finki.ukim.mk>
2024-12-19 23:50:43 +0100

    new

--- src/main/java/mk/ukim/finki/lab1/web/controller/SongController.java
+++ src/main/java/mk/ukim/finki/lab1/web/controller/SongController.java
@@ -5,6 +5,8 @@ import mk.ukim.finki.lab1.model.Song;
 import mk.ukim.finki.lab1.service.AlbumService;
 import mk.ukim.finki.lab1.service.ArtistService;
 import mk.ukim.finki.lab1.service.SongService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -26,9 +28,10 @@ public class SongController {
     }
 
     @GetMapping
-    public String getSongsPage(@RequestParam(required = false) String error, Model model) {
+    public String getSongsPage(@RequestParam(required = false) String error, Model model, Authentication authentication) {
         model.addAttribute("songs", songService.listSongs());
         model.addAttribute("error", error);
+        System.out.println("Authorities: " + authentication.getAuthorities());
         return "listSongs";
     }
 
@@ -42,15 +45,22 @@ public class SongController {
                            @RequestParam String title,
                            @RequestParam String genre,
                            @RequestParam int releaseYear,
-                           @RequestParam Long album) {
+                           @RequestParam Long album, Authentication authentication) {
 
         if (id != null) {
             songService.editSong(id, title, genre, releaseYear, album);
         } else {
             songService.addSong(title, genre, releaseYear, album);
         }
-
+        System.out.println("Authorities /add: " + authentication.getAuthorities());
         return "redirect:/songs";
+    }
+    @GetMapping("/album/{albumId}")
+    public String getSongsByAlbum(@PathVariable Long albumId, Model model) {
+        List<Song> songs = songService.findByAlbumId(albumId);
+        model.addAttribute("songs", songs);
+        model.addAttribute("error", null);
+        return "listSongs";
     }
 
     @GetMapping("/edit-form/{id}")
